334780a0ee9fb72a0eb99e759826bcecb5c4107a
lfariabr <lfariabr@gmail.com>
2024-09-20 16:21:10 +0000

    Updating...

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ if page == "Leads":
   # Group by dia do mês and count unique leads
   groupby_leads_dia_do_mes = df_leads.groupby('apenas_o_dia').agg({'ID do lead': 'nunique'}).reset_index()
   groupby_leads_por_unidade = df_leads.groupby('Unidade').agg({'ID do lead': 'nunique'}).reset_index()
+  groupby_leads_por_fonte = df_leads.groupby('Fonte').agg({'ID do lead': 'nunique'}).reset_index()
+  groupby_leads_por_status = df_leads.groupby('Status').agg({'ID do lead': 'nunique'}).reset_index()
 
   st.write("Número de leads por dia")
 
@@ -55,8 +57,7 @@ if page == "Leads":
   # Display the graph
   st.plotly_chart(graph_dia_do_mes)
 
-  # Create line graph for leads by day of the month
-  
+  # Create bar graph for leads by day of the month
   graph_por_loja = px.bar(
       groupby_leads_por_unidade,
       x='Unidade',
@@ -68,3 +69,26 @@ if page == "Leads":
   # Display the graph
   st.plotly_chart(graph_por_loja)
 
+  # Create pizza chart for leads per font
+  graph_por_fonte = px.pie(
+      groupby_leads_por_fonte,
+      names='Fonte',
+      values='ID do lead',
+      title='Número de Leads por Fonte',
+      labels={'ID do lead': 'Número de Leads', 'Fonte': 'Fonte'},
+  )
+
+  # Display the graph
+  st.plotly_chart(graph_por_fonte)
+
+  # Create pizza graph for leads per status
+  graph_por_status = px.pie(
+      groupby_leads_por_status,
+      names='Status',
+      values='ID do lead',
+      title='Número de Leads por Status',
+      labels={'ID do lead': 'Número de Leads', 'Status': 'Status'},
+  )
+
+  # Display the graph
+  st.plotly_chart(graph_por_status)
