7e25e8f09cb210a3f86c38a34efbabc43fa5ab9b
lfariabr <lfariabr@gmail.com>
2024-11-12 08:21:21 +0000

    Leads GraphQL

--- views/asyncdata.py
+++ views/asyncdata.py
@@ -147,28 +147,196 @@ if submitted:
 
       return all_leads
 
+    # Fetch appointments with pagination + retry logic
+    async def fetch_appointments(session, start_date, end_date, token):
+        current_page = 1
+        all_appointments = []
+
+        while True:
+            query = '''query ($filters: AppointmentFiltersInput, $pagination: PaginationInput) {
+                        fetchAppointments(filters: $filters, pagination: $pagination) {
+                            meta {
+                                currentPage
+                                lastPage
+                            }
+                            data {
+                                id
+                                status {
+                                    label
+                                }
+                                createdBy {
+                                    name
+                                    createdAt
+                                }
+                                store {
+                                    name
+                                }
+                                customer {
+                                    id
+                                    name
+                                    telephones {
+                                        number
+                                    }
+                                }
+                                procedure {
+                                    name
+                                    groupLabel
+                                }
+                                employee {
+                                    name
+                                }
+                                startDate
+                            }
+                        }
+                    }'''
+            variables = {
+                'filters': {
+                    'startDateRange': {
+                        'start': start_date,
+                        'end': extended_end_date,
+                    },
+                },
+                'pagination': {
+                    'currentPage': current_page,
+                    'perPage': 200,  # Large page size to reduce the number of requests
+                },
+            }
+
+            data = await fetch_graphql(session, 'https://open-api.eprocorpo.com.br/graphql', query, variables, token)
+
+            if data is None:
+                print(f"Failed to fetch appointments on page {current_page}. Retrying...")
+                continue  # Retry the loop on failure
+
+            # appointments_data = data['data']['fetchAppointments']['data']
+            # Check if 'customer' object exists in data and contains necessary fields
+            appointments_data = [
+                {
+                    "id": row['id'],
+                    "status_label": row.get('status', {}).get('label', 'N/A') if isinstance(row.get('status'), dict) else 'N/A',
+                    "store_name": row.get('store', {}).get('name', 'N/A') if isinstance(row.get('store'), dict) else 'N/A',
+                    "customer_id": row.get('customer', {}).get('id', 'N/A') if isinstance(row.get('customer'), dict) else 'N/A',
+                    "customer_name": row.get('customer', {}).get('name', 'N/A') if isinstance(row.get('customer'), dict) else 'N/A',
+                    "customer_telephone": (
+                      row.get('customer', {}).get('telephones', [{}])[0].get('number', 'N/A')
+                      if isinstance(row.get('customer'), dict) and row.get('customer', {}).get('telephones')
+                      else 'N/A'
+                  ),
+                    "procedure_name": row.get('procedure', {}).get('name', 'N/A') if isinstance(row.get('procedure'), dict) else 'N/A',
+                    "procedure_group": row.get('procedure', {}).get('groupLabel', 'N/A') if isinstance(row.get('procedure'), dict) else 'N/A',
+                    "employee_name": row.get('employee', {}).get('name', 'N/A') if isinstance(row.get('employee'), dict) else 'N/A',
+                    "createdBy_name": row.get('createdBy', {}).get('name', 'N/A') if isinstance(row.get('createdBy'), dict) else 'N/A',
+                    "createdBy_createdAt": row.get('createdBy', {}).get('createdAt', 'N/A') if isinstance(row.get('createdBy'), dict) else 'N/A',
+                    "startDate": row.get('startDate', 'N/A'),
+                }
+                for row in data['data']['fetchAppointments']['data']
+            ]
+            all_appointments.extend(appointments_data)
+
+            meta = data['data']['fetchAppointments']['meta']
+            last_page = meta['lastPage']
+
+            print(f"Querying Appointments - Page: {current_page}/{last_page} - startDate: {start_date} - endDate: {end_date}")
+
+            if current_page >= last_page:
+                break
+
+            current_page += 1
+            await asyncio.sleep(5)  # Small delay to avoid spamming the server
+
+        return all_appointments
+
+    # Fetch bill charges with pagination and retry logic
+    async def fetch_bill_charges(session, start_date, end_date, token):
+        current_page = 1
+        all_bill_charges = []
+
+        while True:
+            query = '''query ($filters: BillChargeFiltersInput, $pagination: PaginationInput) {
+                    fetchBillCharges(filters: $filters, pagination: $pagination) {
+                        data {
+                            quote {
+                                id
+                                customer {
+                                    id
+                                    name
+                                    taxvat
+                                    email
+                                }
+                                status
+                                bill {
+                                    total
+                                    installmentsQuantity
+                                    items {
+                                        amount
+                                        description
+                                        quantity
+                                    }
+                                }
+                            }
+                            store {
+                                name
+                            }
+                            amount
+                            paidAt
+                            dueAt
+                            isPaid
+                            paymentMethod {
+                                name
+                            }
+                        }
+                        meta {
+                            currentPage
+                            lastPage
+                        }
+                    }
+                }'''
+            variables = {
+                'filters': {
+                    'paidAtRange': {
+                        'start': start_date,
+                        'end': end_date,
+                    }
+                },
+                'pagination': {
+                    'currentPage': current_page,
+                    'perPage': 200,
+                }
+            }
+
+            data = await fetch_graphql(session, 'https://open-api.eprocorpo.com.br/graphql', query, variables, token)
+
+            if data is None:
+                print(f"Failed to fetch bill charges on page {current_page}. Retrying...")
+                continue  # Retry the loop on failure
+
+            bill_charges_data = data['data']['fetchBillCharges']['data']
+            all_bill_charges.extend(bill_charges_data)
+
+            meta = data['data']['fetchBillCharges']['meta']
+            print(f"Querying Bill Charges - Page: {current_page}/{meta['lastPage']} - startDate: {start_date} - endDate: {end_date}")
+
+            if current_page >= meta['lastPage']:
+                break
+
+            current_page += 1
+            await asyncio.sleep(5)  # Small delay
+
+        return all_bill_charges
+
+    # Main async function to fetch all data concurrently
     async def fetch_all_data(start_date, end_date, extended_end_date, token):
         async with aiohttp.ClientSession() as session:
-            leads_task = fetch_all_leads(session, start_date, end_date, token)
-            leads_data = await asyncio.gather(leads_task)
+            leads_task = fetch_all_leads(session, start_date, end_date, token)  # Use original end_date for leads
+            appointments_task = fetch_appointments(session, start_date, extended_end_date, token)  # Use extended_end_date for appointments
+            bill_charges_task = fetch_bill_charges(session, start_date, end_date, token)  # Use original end_date for bill charges
 
-            return leads_data
+            leads_data, appointments_data, bill_charges_data = await asyncio.gather(
+                leads_task, appointments_task, bill_charges_task
+            )
+
+            return leads_data, appointments_data, bill_charges_data
 
     # Wrapper function to run async functions
     def run_fetch_all(start_date, end_date, extended_end_date, token):
         return asyncio.run(fetch_all_data(start_date, end_date, extended_end_date, token))
-
-    start_time = time.time()
-    # Fetch all data
-    leads_data = run_fetch_all(start_date_str, end_date_str, extended_end_date_str, token)
-    end_time = time.time()
-
-    # Convert to DataFrame and print results
-    if leads_data and leads_data[0]:
-        df_leads = pd.DataFrame(leads_data[0])
-        st.write(f"Leads DataFrame: {df_leads.shape[0]} rows")
-        st.write(df_leads)
-    else:
-        st.write("No data fetched or an error occurred during data fetching.")
-
-    st.write(f"Total time taken: {end_time - start_time:.2f} seconds")
