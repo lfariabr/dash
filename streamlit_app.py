6fa1131369ca79804e160a1639fd1302f97a81d8
lfariabr <lfariabr@gmail.com>
2024-11-13 07:09:04 +0000

    All Data GraphQL

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,6 @@ st.sidebar.write("É possível fazer o download dos dados e visualizar os result
 
 # Condicional para executar páginas específicas com base na escolha
 if choice == 'Conferir dados':
-    # Carregar dados uma vez e usá-los para preencher as opções de filtro
-    df_leads = leads.load_data_from_gsheet()
-
-    # Criação de filtros no sidebar
-    all_sources = df_leads['source'].dropna().unique()
-    all_stores = df_leads['store'].dropna().unique()
-    all_categories = df_leads['category'].dropna().unique()
-
-    # Opções de filtro com 'Todos' incluído
-    selected_sources = st.sidebar.multiselect('Select Source', options=all_sources, default=all_sources)
-    selected_stores = st.sidebar.multiselect('Select Store', options=all_stores, default=all_stores)
-    selected_categories = st.sidebar.multiselect('Select Category', options=all_categories, default=all_categories)
-
-    # Executar a função run do módulo leads, passando os filtros selecionados
-    leads.run(selected_sources, selected_stores, selected_categories)
+    leads.run()
 elif choice == 'Baixar dados':
     asyncdata.run()
