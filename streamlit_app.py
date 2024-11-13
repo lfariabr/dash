aa9961160842fe321da7845bcf26728f07d31b13
lfariabr <lfariabr@gmail.com>
2024-11-13 06:57:37 +0000

    All Data GraphQL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,25 @@
 
 import streamlit as st
 
-# --- PAGE SETUP ---
-graphics = st.Page(
-    "views/leads.py",
-    title="Conferir dados",
-    icon="📊",
-)
+# Importação dos scripts de página
+from views import leads, asyncdata
 
-asyncdata = st.Page(
-    "views/asyncdata.py",
-    title="Baixar dados",
-    icon="📈",
-)
+# Configuração da página
+st.set_page_config(page_title="Dashboard de Leads", layout="wide")
 
-# --- NAVIGATION SETUP [WITH SECTIONS] ---
-pg = st.navigation(
-    {
-        "📁 Menu": [asyncdata, graphics],
-    }
-)
+# --- SIDEBAR SETUP ---
+st.sidebar.markdown("# 📁 Menu")
+# Criação dos botões de rádio para a navegação
+choice = st.sidebar.radio("Escolha uma página:", ['Conferir dados', 'Baixar dados'])
 
 st.sidebar.markdown("### Notas")
 st.sidebar.write("É possível fazer o download dos dados e visualizar os resultados em tempo real!")
 
-# --- RUN NAVIGATION ---
-pg.run()
+# Condicional para executar páginas específicas com base na escolha
+if choice == 'Conferir dados':
+    leads.run()
+elif choice == 'Baixar dados':
+    asyncdata.run()
+
+# As funções run() em leads.py e asyncdata.py precisarão ser definidas
+# para incluir toda a lógica que estava anteriormente dentro do contexto da página.
