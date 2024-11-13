f13f6176afe45034f7d3cb07dc2866afe98137c1
lfariabr <lfariabr@gmail.com>
2024-11-13 07:15:41 +0000

    All Data GraphQL

--- views/leads.py
+++ views/leads.py
@@ -44,21 +44,21 @@ def run():
   # Criar filtros com seleção múltipla para 'source', 'store' e 'category'
   col5, col6, col7 = st.columns(3)
   with col5:
-    all_sources = ['TUDO'] + list(df_leads['source'].unique())
-    selected_sources = st.multiselect('Select Source', options=all_sources, default='TUDO')
-    if 'TUDO' in selected_sources:
-        selected_sources = all_sources[1:]  # Exclui "TUDO" para usar todas as fontes
+    all_sources = ['Todas'] + list(df_leads['source'].unique())
+    selected_sources = st.multiselect('Select Source', options=all_sources, default='Todas')
+    if 'Todas' in selected_sources:
+        selected_sources = all_sources[1:]  # Exclui "Todas" para usar todas as fontes
 
   with col6:
-      all_stores = ['TUDO'] + list(df_leads['store'].unique())
-      selected_stores = st.multiselect('Select Store', options=all_stores, default='TUDO')
-      if 'TUDO' in selected_stores:
-          selected_stores = all_stores[1:]  # Exclui "TUDO" para usar todas as lojas
+      all_stores = ['Todas'] + list(df_leads['store'].unique())
+      selected_stores = st.multiselect('Select Store', options=all_stores, default='Todas')
+      if 'Todas' in selected_stores:
+          selected_stores = all_stores[1:]  # Exclui "Todas" para usar todas as lojas
 
   with col7:
-      all_categories = ['TUDO'] + list(df_leads['category'].unique())
-      selected_categories = st.multiselect('Select Category', options=all_categories, default='TUDO')
-      if 'TUDO' in selected_categories:
+      all_categories = ['Todas'] + list(df_leads['category'].unique())
+      selected_categories = st.multiselect('Select Category', options=all_categories, default='Todas')
+      if 'Todas' in selected_categories:
           selected_categories = all_categories[1:]  # Exclui "TUDO" para usar todas as categorias
     
   # Aplicar filtros
