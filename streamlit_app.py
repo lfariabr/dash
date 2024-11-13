02bd6ff478ccf5420d181070e046b3a7644699b6
lfariabr <lfariabr@gmail.com>
2024-11-13 07:04:14 +0000

    All Data GraphQL

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,15 +17,6 @@ st.sidebar.write("É possível fazer o download dos dados e visualizar os result
 
 # Condicional para executar páginas específicas com base na escolha
 if choice == 'Conferir dados':
-    
-  col5, col6, col7 = st.columns(3)
-  with col5:
-    selected_sources = st.multiselect('Select Source', options=df_leads['source'].unique(), default=df_leads['source'].unique())
-  with col6:
-    selected_stores = st.multiselect('Select Store', options=df_leads['store'].unique(), default=df_leads['store'].unique())
-  with col7:
-    selected_categories = st.multiselect('Select Category', options=df_leads['category'].unique(), default=df_leads['category'].unique())
-
     leads.run()
 elif choice == 'Baixar dados':
     asyncdata.run()
